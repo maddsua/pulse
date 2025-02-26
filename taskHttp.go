9f3af0ec6cfdad3cd11f02c0bcba46c40fdd02ee
maddsua <maddsuadev@pm.me>
2025-02-26 22:20:48 +0100

    Update taskHttp.go

--- taskHttp.go
+++ taskHttp.go
@@ -76,7 +76,6 @@ func NewHttpTask(label string, opts HttpProbeConfig, proxies ProxyConfigMap) (*h
 			}
 		}
 
-		//	todo: make sure proxy can reconnect
 		dialer, err := proxy.SOCKS5("tcp", proxyUrl.Host, proxyAuth, proxy.Direct)
 		if err != nil {
 			return nil, fmt.Errorf("failed to create proxy dialer: %s", err.Error())
