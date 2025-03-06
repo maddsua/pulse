71b1d1626dade61ff18e708d3300b1b3d53a9072
maddsua <maddsuadev@pm.me>
2025-03-07 00:29:06 +0100

    Update aggregate.go

--- exporters/aggregate.go
+++ exporters/aggregate.go
@@ -12,5 +12,29 @@ func aggregateUptimeEntries(entries []storage.UptimeEntry, interval time.Duratio
 		return entries
 	}
 
-	//	todo: do the aggregation
+	var result []storage.UptimeEntry
+
+	var group []storage.UptimeEntry
+	groupTime := entries[0].Time
+
+	for _, entry := range entries {
+
+		if entry.Time.Sub(groupTime) > interval {
+			result = append(result, mergeUptimeEntries(group))
+			group = []storage.UptimeEntry{}
+			groupTime = entry.Time
+		}
+
+		group = append(group, entry)
+	}
+
+	if len(group) > 0 {
+		result = append(result, mergeUptimeEntries(group))
+	}
+
+	return result
+}
+
+func mergeUptimeEntries(entries []storage.UptimeEntry) storage.UptimeEntry {
+	//	todo: group
 }
