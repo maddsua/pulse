76bf791a67d031495122eb5962474f6ebccd26d5
maddsua <maddsuadev@pm.me>
2025-03-07 00:35:15 +0100

    Update aggregate.go

--- exporters/aggregate.go
+++ exporters/aggregate.go
@@ -20,7 +20,7 @@ func aggregateUptimeEntries(entries []storage.UptimeEntry, interval time.Duratio
 	for _, entry := range entries {
 
 		if entry.Time.Sub(groupTime) > interval {
-			result = append(result, mergeUptimeEntries(group))
+			result = append(result, mergeLabeledUptimeEntries(group)...)
 			group = []storage.UptimeEntry{}
 			groupTime = entry.Time
 		}
@@ -29,12 +29,27 @@ func aggregateUptimeEntries(entries []storage.UptimeEntry, interval time.Duratio
 	}
 
 	if len(group) > 0 {
-		result = append(result, mergeUptimeEntries(group))
+		result = append(result, mergeLabeledUptimeEntries(group)...)
+	}
+
+	return result
+}
+
+func mergeLabeledUptimeEntries(entries []storage.UptimeEntry) []storage.UptimeEntry {
+
+	byLabel := map[string][]storage.UptimeEntry{}
+	for _, entry := range entries {
+		byLabel[entry.Label] = append(byLabel[entry.Label], entry)
+	}
+
+	var result []storage.UptimeEntry
+	for _, labelEntries := range byLabel {
+		result = append(result, mergeUptimeEntries(labelEntries))
 	}
 
 	return result
 }
 
 func mergeUptimeEntries(entries []storage.UptimeEntry) storage.UptimeEntry {
-	//	todo: group by label
+	//	todo: group data
 }
